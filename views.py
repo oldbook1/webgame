f09e603b48b4dbf8a9bc21257eb45ed8f3eb3414
betain24 <betain24@naver.com>
2016-06-09 23:43:25 +0900

    게임로직
    상대방의 숫자를 알아오는게 정확하게 되있지않아서 그냥 주석처리하고 올림

--- views.py
+++ views.py
@@ -26,8 +26,48 @@ def startpage(request):
 
 	return render(request,"home.html",{ "form":form, })
 
-def game(request):
-	return render(request,"game.html")	
+def game1(request):
+	if request.method == "POST"
+		form = form(request.POST)
+		my_number = form.cleaned_data.get("my_number")
+		my_number.save()
+		return render(request, "game2.html")
+
+def game2(request):
+	if request.method == "POST":
+		form = Form(request.POST)
+		guess_number = form.cleanead_data.get("guess_numbber")
+		#otehr_number = player2.my_number
+		ball = 0, strike =0
+		a = guess_number // 1000; guess_number -= a * 1000;
+		b = guess_number // 100; guess_number -= b * 100;
+		c = guess_number // 10; guess_number -= c * 10;
+		d = guess_number;
+		guess_list = [a,b,c,d]
+		"""
+		e = other_number // 1000; other_number -= e * 1000;
+		f = other_number // 100; other_number -= g * 100;
+		g = other_number // 10; other_number -= h * 10;
+		h = other_number;
+		other_list = [e, f, g, h]
+		for i in range(4):
+			for j in range(4):
+				if other_list[i] == guess_number[j]:
+					ball++
+		for in in range(4):
+			if other_list[i] == guess_number[i]:
+				ball--
+				strike++
+		states = str(strike) + "스트라이커" + str(ball) + "볼"
+		states 디비에 저장
+
+		if strike==4:
+			return render(request, "win.html")
+		else:
+			return render(requst, "game2.html", {form을 리턴하면 방정보같은 쓸데없는게 나갈거같지만 일단 form을 리턴})
+		"""
+
+	return render(request,"game2.html")	
 
 
 def waits(request):
