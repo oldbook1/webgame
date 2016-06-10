5cecfca30558902046e7268200054d1f89025f46
oldbook1 <limchan1@naver.com>
2016-06-10 20:58:48 +0900

    게임 미완성
    게임 model,form,db 구성함
    
    문제 game1.html form_num.as_p가 안됨 form_num에서 my_number만 입력받으면 되는데 해결할 수
    있으면 해줘
    
    해야할것
    1. 게임완성
    2, 게임통신
    3. 템플릿 꾸미기
    
    추가
    1. 방 비밀번호설정
    2. 빈방 출력

--- game/forms.py
+++ game/forms.py
@@ -13,3 +13,113 @@ class Form(forms.ModelForm):
 	def clean_room(self):
 		room = self.cleaned_data.get('room')
 		return room
+
+
+class Form_mynum(forms.ModelForm):
+	class Meta:
+		model = player_num 
+		fields = ['my_number','room']
+
+	def clean_my_number(self):
+		nicname = self.cleaned_data.get('my_number')
+		return my_number
+
+
+class Form_numberList_room1(forms.ModelForm):
+	class Meta:
+		model = number_room1
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room2(forms.ModelForm):
+	class Meta:
+		model = number_room2
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room3(forms.ModelForm):
+	class Meta:
+		model = number_room3
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room4(forms.ModelForm):
+	class Meta:
+		model = number_room4
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room5(forms.ModelForm):
+	class Meta:
+		model = number_room5
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room6(forms.ModelForm):
+	class Meta:
+		model = number_room6
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room7(forms.ModelForm):
+	class Meta:
+		model = number_room7
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room8(forms.ModelForm):
+	class Meta:
+		model = number_room8
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room9(forms.ModelForm):
+	class Meta:
+		model = number_room9
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
+
+
+class Form_numberList_room10(forms.ModelForm):
+	class Meta:
+		model = number_room10
+		fields = ['guess_number','number_judgment']
+	
+	def clean_guess_number(self):
+		guess_number = self.cleaned_data.get('guess_number')
+		return guess_number
