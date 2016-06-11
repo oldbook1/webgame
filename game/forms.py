60054e809e1fc6e2a3ec12c2dafa8fb9971cea5d
oldbook1 <limchan1@naver.com>
2016-06-11 23:54:46 +0900

    게임
    오류 가 많음
    1. 원하는 입력만 받는 템플릿들 수정
    2. 게임통신 (턴방식등,새로고침듬)

--- game/forms.py
+++ game/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class Form(forms.ModelForm):
 	class Meta:
 		model = player
-		fields = ['nicname','room']
+		fields = ['nicname','room','cnt']
 	
 	def clean_nicname(self):
 		nicname = self.cleaned_data.get('nicname')
@@ -14,112 +14,42 @@ class Form(forms.ModelForm):
 		room = self.cleaned_data.get('room')
 		return room
 
+	def clean_cnt(self):
+		cnt = self.cleaned_data.get('cnt')
+		return cnt
+
 
 class Form_mynum(forms.ModelForm):
 	class Meta:
 		model = player_num 
-		fields = ['mynumber','room']
+		fields = ['mynumber','room','cnt']
 
 	def clean_mynumber(self):
 		mynumber = self.cleaned_data.get('mynumber')
 		return mynumber
 
+	def clean_room(self):
+		room = self.cleaned_data.get('room')
+		return room
 
-class Form_numberList_room1(forms.ModelForm):
+	def clean_cnt(self):
+		cnt = self.cleaned_data.get('cnt')
+		return cnt
+
+
+class Form_numberList_room(forms.ModelForm):
 	class Meta:
-		model = number_room1
-		fields = ['guess_number','number_judgment']
+		model = number_room
+		fields = ['guess_number','ball','strike']
 	
 	def clean_guess_number(self):
 		guess_number = self.cleaned_data.get('guess_number')
 		return guess_number
 
+	def clean_ball(self):
+		ball = self.cleaned_data.get('ball')
+		return ball
 
-class Form_numberList_room2(forms.ModelForm):
-	class Meta:
-		model = number_room2
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room3(forms.ModelForm):
-	class Meta:
-		model = number_room3
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room4(forms.ModelForm):
-	class Meta:
-		model = number_room4
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room5(forms.ModelForm):
-	class Meta:
-		model = number_room5
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room6(forms.ModelForm):
-	class Meta:
-		model = number_room6
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room7(forms.ModelForm):
-	class Meta:
-		model = number_room7
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room8(forms.ModelForm):
-	class Meta:
-		model = number_room8
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room9(forms.ModelForm):
-	class Meta:
-		model = number_room9
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
-
-
-class Form_numberList_room10(forms.ModelForm):
-	class Meta:
-		model = number_room10
-		fields = ['guess_number','number_judgment']
-	
-	def clean_guess_number(self):
-		guess_number = self.cleaned_data.get('guess_number')
-		return guess_number
+	def clean_strike(self):
+		strike = self.cleaned_data.get('strike')
+		return strike
